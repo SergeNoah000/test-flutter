aa4d319bdbeb5117dd40a9d1e6bf845728257b13
SergeNoah000 <gaetan.noah@facsciences-uy1.cm>
2024-03-22 10:29:51 +0100

    Ca fonctionne ! Félicitations..

--- lib/main.dart
+++ lib/main.dart
@@ -28,10 +28,10 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor:  const Color.fromARGB(255, 45, 248, 255)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Si tu es ici  alors sache que flutter fontionne bien'),
     );
   }
 }
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Button cliqué: ',
             ),
             Text(
               '$_counter',
